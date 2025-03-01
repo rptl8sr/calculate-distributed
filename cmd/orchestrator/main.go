37cf04f287ad95e33109ac0e733280642b7d433b
rptl8sr <rptl8sr@gmail.com>
2025-03-01 11:42:32 +0300

    Add orchestrator run

--- cmd/orchestrator/main.go
+++ cmd/orchestrator/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	"calculate-distributed/internal/api"
+	"calculate-distributed/internal/server"
+)
+
+func main() {
+	si := server.New()
+
+	r := chi.NewMux()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	h := api.HandlerFromMux(si, r)
+
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: h,
+	}
+
+	fmt.Println("Starting server on port 8080")
+	if err := s.ListenAndServe(); err != nil {
+		fmt.Println(err)
+	}
+}
